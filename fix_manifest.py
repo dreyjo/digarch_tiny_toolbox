82df5c059f7b653753b1a8e190c881776c73a827
dreyjo <59994482+dreyjo@users.noreply.github.com>
2022-07-08 11:50:09 -0400

    edited to remove and validate removal of lines

--- fix_manifest.py
+++ fix_manifest.py
@@ -1,27 +1,59 @@
-#!/usr/bin/env python3
+#!/usr/local/bin/ python3
 
 import re
 import sys
 import os
 
 def clean_manifest(textfile):
-    patterns = ['._', '.DS_Store', '.ds_store', '.DS_STORE', '~.', '~']
+    patterns = ['._', '.DS_Store', '.ds_store', '.DS_STORE', '~.', '~', '.~']
     linecount = 0
     delete = []
+    val = []
+
 
     #open file to read lines
-    i =open(textfile, "r")
+    with open(textfile, 'r+') as fp:
+    # i =open(textfile, "r+")
     # with open(textfile, 'r') as input:
-    lines = i.readlines()
-    i.close()
+        lines = fp.readlines()
+        pre = len(lines)
 
-    #open file to write
-    i=open(textfile, "w")
-    for line in lines:
-        if '._' not in line:
-            i.write(line)
-    i.close()
+        for line in lines:
+            for pattern in patterns:
+                if pattern in line:
+                    delete.append(line)
+                else:
+                    val.append(line)
 
+    # print(len(delete))
+    # print(delete)
+        fp.seek(0)
+        fixed = 0
+        for line in lines:
+        #     for d in delete:
+            if line not in delete:
+                fp.write(line)
+                fixed = fixed + 1
+                fp.truncate()
+
+        def validate(lines, pre_count, fixed_count, delete_list, validate_list):
+            expected = pre_count-len(delete_list)
+            x = set(lines)
+            y = set(validate_list)
+
+            print(f'{pre_count} lines before fixing')
+            print(f'{len(delete_list)} lines to be removed')
+            print(f'expecting {expected} lines')
+            print(f'{fixed_count} lines in fixed manifest')
+
+            if fixed_count == expected:
+                print('expected line count matches')
+
+            if x == y:
+                print('expceted fixed lines present in manifest')
+
+
+        validate(lines, pre, fixed, delete, val)
 
 
 def main():
